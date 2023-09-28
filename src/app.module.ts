75d4f8abef93739cef77be286e82ffadef1f74ce
Yubron <wlstn4966@gmail.com>
2023-09-28 17:55:38 +0900

    ✨feat: "health check api"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from './modules/common/common.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [],
   providers: [],
 })
