e139f52a9830167a75bc6ef9bef89f586238fc9b
Yubron <wlstn4966@gmail.com>
2023-09-29 16:07:57 +0900

    ✨feat: "connect db"
    - using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './modules/common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmConfigService } from './modules/common/database/typeorm.config.service';
 
 @Module({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (
+        options?: DataSourceOptions,
+      ): Promise<DataSource> => {
+        if (!options) throw new Error('options is undefined');
+        return await new DataSource(options).initialize();
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/common/database/typeorm.config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('DB_HOST'),
+      port: +this.configService.get('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PW'),
+      database: this.configService.get('DB_NAME'),
+      synchronize: false,
+      namingStrategy: new SnakeNamingStrategy(),
+      logging: true,
+      entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+    } as TypeOrmModuleOptions;
+  }
+}
